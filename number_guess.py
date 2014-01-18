7ea54d0bc576b912a6ab6f628b00118a23d3096d
sjm4584 <sjm4584@rit.edu>
2014-01-17 21:20:27 -0500

    cleaned up and added better comments

--- number_guess.py
+++ number_guess.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 from random import randint
 
-
-
 change = ""
 high = 1000
 low = 1
 guesses = 0
-while(change != "done"):
-   guess = (high+low) / 2
-   print "Is it: ", guess
-   change = raw_input("Higher or lower? ")
 
-   if change == "higher":
-      low = guess
-   elif change == "lower":
-      high = guess
+while(change.lower() != "done"):
+   guess = (high+low) / 2
+   print "Is your number: ", guess
+   change = raw_input("Am I close? (higher or lower)")
+
+   if change.lower() == "higher":
+      low = guess #b/c you know it will never be below this number
+   elif change.lower() == "lower":
+      high = guess #b/c you know it will never be above this number
+      
    guesses = guesses + 1
-   print '-'*25
+   print '-'*25 #prints 25 '-' characters
    
-print "guessed in: ", guesses
+print "Ha! I guessed your number in only ", guesses
